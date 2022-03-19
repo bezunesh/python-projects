a80651e549ac55b15adbc804c9419f17aa22404e
Bezunesh <zueb21@gmail.com>
2022-03-19 13:53:36 -0400

    feat: Adds more unittests

--- amharic-wordle/tests.py
+++ amharic-wordle/tests.py
@@ -1,6 +1,6 @@
 import unittest
 import pandas as pd
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from random import choice
 from wordle import AmharicWordle
 
@@ -74,6 +74,40 @@ class WordleTest(unittest.TestCase):
         resulting_style_df = self.game.highight([], row=row, greens=greens, oranges=oranges, grays=grays)
         self.assertTrue(df.equals(resulting_style_df))
 
+    @patch('wordle.display')
+    @patch('wordle.pd.DataFrame')
+    @patch('wordle.input')
+    def test_run_main(self, mock_input, mock_pd_dataframe, mock_display):
+        self.game.set_word_of_the_day = Mock(return_value='lucky')
+        mock_input.return_value = 'lobby'
+        self.game.chances = 1
+        row = 0
+        # the userinput's the 1st and last characters are correct(green), while the rest not (grays)
+        greens, oranges, grays = [0,4],[],[1,2,3]
+        self.game.set_colors = Mock(return_value=(greens, oranges, grays))
+        remaining_attempts = ['','','','','']
+        self.game.remaining_attempts = Mock(return_value=[remaining_attempts])
+        df_data = [['l','o','b','b','y'],remaining_attempts]
+        df = pd.DataFrame('', columns=[0,1,2,3,4], indexs=[0,1,2,3,4,5])
+        mock_pd_dataframe.return_value = df
+        df.style.apply = Mock(return_value='')
+    
+        self.game.run_main()
+        
+        mock_input.assert_called_with("Guess a 5 letter word: ")
+        self.assertEqual(self.game.attempts, [['l','o','b','b','y']])
+        self.game.set_colors.assert_called_with('lobby')
+        mock_pd_dataframe.assert_called_with(data=df_data)
+        df.style.apply.assert_called_with(
+            self.game.highight,
+            axis=None,
+            greens = greens,
+            oranges=oranges,
+            grays=grays,
+            row=row
+        )
+        mock_display.assert_called_with('')
+    
 
 if __name__ == '__main__':
    unittest.main()
