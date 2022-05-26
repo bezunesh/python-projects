432f7618e7cc2ca65777f3b913d4b0e84d5a2ea9
Bezunesh <zueb21@gmail.com>
2022-05-26 15:23:13 -0400

    Adds more features to the fakestoreapi

--- apis/wordoftheday/main/api/fakestoreapi.py
+++ apis/wordoftheday/main/api/fakestoreapi.py
@@ -1,31 +1,90 @@
+from __future__ import annotations
 from urllib import request
 from urllib.error import HTTPError, URLError
-from urllib.parse import urlencode
-import certifi
-import ssl
-import json
+from urllib.parse import urlencode, quote
+import certifi, ssl, json
+from .products import Product
 
-api_url = "https://fakestoreapi.com"
-path = "/products"
-limit = 2
-queryString = urlencode({'limit': limit})
-url = f"{api_url}{path}?{queryString}"
-context = ssl.create_default_context(cafile=certifi.where())
+class EcommerceApi:
+    def __init__(self):
+        self.api_url = "https://fakestoreapi.com"
+        self.headers = {'User-Agent': 'Mozilla/5.0'}
+        self.context = ssl.create_default_context(cafile=certifi.where())
+    
+    def sendRequest(self, path, limit=None, sort=None):
+        d, queryString = {}, None
+        if limit: d['limit'] = limit
+        if sort: d['sort'] = sort
+        if d: queryString = urlencode(d)
+        url = f"{self.api_url}{path}?{queryString}" if queryString else f"{self.api_url}{path}"
+        try:
+            requestObj = request.Request(url, headers=self.headers)
+            with request.urlopen(requestObj, context=self.context) as response:
+                # convert response to a python list object
+                parsed_python_obj = json.load(response)
+                # convert python list object to  a json formatted str
+                #serialized_python_obj = json.dumps(parsed_python_obj, indent=2)  
+                return parsed_python_obj    
+        except HTTPError as e:
+            print(e)
+        except URLError as e:
+            print(e)
+    
+    def getProducts(self, limit=10, sort='asc') -> str:
+        path = "/products"
+        return self.sendRequest(path, limit=limit, sort=sort)
+    
+    def getProduct(self, id) ->str:
+        path = f"/products/{id}"
+        url = f"{self.api_url}{path}"
+        return self.sendRequest(path)
+    
+    def getCategories(self, limit=None, sort=None) -> list[str]:
+        """
+        Returns a list of product categories
+        """
+        path = "/products/categories"
+        return self.sendRequest(path, limit, sort)
 
-try:
-    requestObj = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    with request.urlopen(requestObj, context=context) as response:
-        # convert response to a python list object
-        parsed_python_obj = json.load(response)
-        # convert python list object to  a json formatted str, 
-        # pretty print too
-        serialized_python_obj = json.dumps(parsed_python_obj, indent=2)
-        print(parsed_python_obj)
-        print(type(parsed_python_obj))
-        print(serialized_python_obj)
-        print(type(serialized_python_obj))
-      
-except HTTPError as e:
-    print(e)
-except URLError as e:
-    print(e)
+    def getProductsInCategory(self, category: str, limit: int = None, sort: str = None) -> str:
+        path = quote(f"/products/category/{category}")
+        return self.sendRequest(path, limit, sort)
+    
+    def addNewProduct(self, product: Product):
+        path = "/products"
+        url = f"{self.api_url}{path}"
+        data = json.dumps(product.__dict__)
+        req_obj = request.Request(url, data=data.encode(), method="POST", headers=self.headers)
+        try:
+            with request.urlopen(req_obj, context=self.context) as response:
+                content_type = response.getheader('Content-Type')
+                print(content_type)
+                if content_type.startswith("application/json"):
+                    # parse it to a python obj
+                    parsed_data = json.load(response)
+                    return parsed_data
+        except (HTTPError, URLError) as e:
+            print(e)
+
+
+def addNewProduct():
+    api = EcommerceApi()
+    product = Product('Addidas sneaker', 14.5, 'New release sneaker', 
+        'https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg', 'men\'s clothing')
+    new_product = api.addNewProduct(product)
+    print(new_product)
+
+def getAllCategories():
+    api = EcommerceApi()
+    categories = api.getCategories()
+    # get two products from each category
+    for category in categories:
+        products = api.getProductsInCategory(category, limit=2)
+        print(category)
+        print("********")
+        print(products)
+
+if __name__ == '__main__':
+    # add new product
+    addNewProduct()
+
---
+++ apis/wordoftheday/main/api/products.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, title, price, description, image, category):
+        self.title = title
+        self.price = price
+        self.description = description
+        self.image = image
+        self.category = category
